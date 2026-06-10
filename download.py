8aebb16e1f0d1c22ed96a2b79ce0719af8c2b97b
tommasopantano01 <s349341@studenti.polito.it>
2026-06-10 18:07:17 +0200

    Update download.py

--- download.py
+++ download.py
@@ -4,9 +4,9 @@ import argparse
 
 FILES = {
     "snapshots_train.npy":       "https://drive.google.com/file/d/1lD7m7UGnuQMal1JEVLJZap_5twEH3haV/view?usp=sharing",
-    "snapshots_train_enriched":  "https://drive.google.com/file/d/1oaVh8KNTfPoFYOPUkTGdhzJvhufEjnRj/view?usp=sharing",
+    "snapshots_train_enriched.npy":  "https://drive.google.com/file/d/1oaVh8KNTfPoFYOPUkTGdhzJvhufEjnRj/view?usp=sharing",
     "parameters_train.npy":      "https://drive.google.com/file/d/1Rf74LJTKBv1C0H9XdkefZMk3RViNqZ-X/view?usp=sharing",
-    "parameters_train_enriched": "https://drive.google.com/file/d/1hHB54xZW_CuW4Jdk7swTsoq2FQ5bkHyn/view?usp=sharing",
+    "parameters_train_enriched.npy": "https://drive.google.com/file/d/1hHB54xZW_CuW4Jdk7swTsoq2FQ5bkHyn/view?usp=sharing",
     "snapshots_test.npy":        "https://drive.google.com/file/d/142IwZF8k5r8paXtUjZ-wZ_7ZLK9Puyqt/view?usp=sharing",
     "parameters_test.npy":       "https://drive.google.com/file/d/1oZ1mwEc1XY0Wwcmi_dVvCb2WIfAu8sof/view?usp=sharing",
 }
